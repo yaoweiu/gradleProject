e85f6ed96e5f87b2dc3c36a522db35a96fc3af34
yaowei <yaowei@ipsy.com>
2020-02-02 18:35:31 -0800

    [Jackson] POJOs

--- src/test/groovy/com/ipsy/yaowei/pojos/ATest.groovy
+++ src/test/groovy/com/ipsy/yaowei/pojos/ATest.groovy
@@ -1,12 +1,20 @@
 package com.ipsy.yaowei.pojos
 
 import spock.lang.Specification
+
+import java.text.SimpleDateFormat
+import java.time.Duration
+import java.time.LocalDate
+import java.time.ZoneOffset
+
 import static com.ipsy.yaowei.Utils.*
 
 class ATest extends Specification {
   def "object mapper"() {
     given:
     def input = getClass().classLoader.getResourceAsStream("base.json")
+    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+    Date date = formatter.parse("2020-01-01T09:00:00+0000")
     when:
     A a = OBJECT_MAPPER.readValue(input, A)
     then:
@@ -17,6 +25,11 @@ class ATest extends Specification {
     assert a.base.aFloat == 4.0
     assert a.base.aDouble == 5.0
 
-    assert a.dateTime.date == new Date("2020-01-01T09:00:00.000Z")
+    assert a.dateTime.date == date
+    assert a.dateTime.duration == Duration.ofMinutes(20)
+    assert a.dateTime.instant == date.toInstant()
+    assert a.dateTime.localDate == new LocalDate(2020,1 ,1)
+    assert a.dateTime.localDateTime.toInstant(ZoneOffset.UTC) == date.toInstant()
+    assert a.dateTime.zonedDateTime.toInstant() == date.toInstant()
   }
 }
