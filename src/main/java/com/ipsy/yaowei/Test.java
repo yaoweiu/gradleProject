5207082edf3e52bbe024ad63949125c8d6a57488
Yaowei <yaowei@ipsy.com>
2020-02-04 17:58:18 -0800

    [JsonSchema] add a test

--- src/main/java/com/ipsy/yaowei/Test.java
+++ src/main/java/com/ipsy/yaowei/Test.java
@@ -1,10 +1,18 @@
 package com.ipsy.yaowei;
 
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.InputStream;
+import java.net.URL;
 
 @Slf4j
 public class Test {
-  public static void main(String []argv) {
+  public static void main(String []argv) throws Exception {
     log.info("Hello world");
+    JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
+
+    URL input = Test.class.getClassLoader().getResource("lifecycle_audit.json");
+    factory.getJsonSchema(input.toString());
   }
 }
